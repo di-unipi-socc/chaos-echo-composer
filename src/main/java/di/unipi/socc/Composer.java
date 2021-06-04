b6aba3a476c653b7fc9ebfd1dd8c5234ba47a91a
jsoldani <jsoldani@gmail.com>
2021-06-04 17:24:42 +0200

    service dependencies on elk stack too

--- src/main/java/di/unipi/socc/Composer.java
+++ src/main/java/di/unipi/socc/Composer.java
@@ -21,20 +21,23 @@ public final class Composer {
     private static int DEFAULT_PICK_PERCENTAGE = 50;
     private static int DEFAULT_FAIL_PERCENTAGE = 5;
 
+    private static int DEFAULT_REPLICAS = 1;
+
     private static String ELK_YAML = "configs/elk.yml";
+
     public static void main(String[] args) {
         // Parse arguments
         String inputFile = args[0];
-        InputStream inputStream = null;;
+        InputStream inputStream = null;
         try {
             inputStream = new FileInputStream(new File(inputFile));
         } catch (FileNotFoundException e) {
             System.err.println("ERROR: Input file not found");
             return;
-        } 
+        }
 
         String outputFile = args[1];
-        FileWriter writer = null;;
+        FileWriter writer = null;
         try {
             writer = new FileWriter(new File(outputFile));
         } catch (IOException e) {
@@ -42,81 +45,99 @@ public final class Composer {
             return;
         }
 
-        // Parse input file
+        // Parse input file and get "services"
         Yaml yaml = new Yaml();
-        Map<String,Object> spec = yaml.load(inputStream); 
+        Map<String, Object> spec = yaml.load(inputStream);
+        Map<String, Object> services = (Map<String, Object>) spec.get("services");
+        for (String serviceName : services.keySet())
+            if (services.get(serviceName) == null)
+                services.put(serviceName, new HashMap<String, Object>());
+
+        // Parse ELK file and get "elkServices"
+        InputStream elkStream = null;
+        try {
+            elkStream = new FileInputStream(new File(ELK_YAML));
+        } catch (FileNotFoundException e) {
+            System.err.println("ERROR: Input file not found");
+            return;
+        }
+        Map<String, Object> elk = yaml.load(elkStream);
+        Map<String, Object> elkServices = (Map<String, Object>) elk.get("services");
 
         // Add Docker Compose Version
         spec.put("version", "3.8");
 
-        // Get services (and create empty maps for services "just named")
-        Map<String,Object> services = (Map<String,Object>) spec.get("services");
-        for(String serviceName : services.keySet())
-            if(services.get(serviceName) == null) 
-                services.put(serviceName,new HashMap<String,Object>());
-
         // Complete services
-        for(String serviceName : services.keySet()) {
-            Map<String,Object> service = (Map<String,Object>) services.get(serviceName);
-            
+        for (String serviceName : services.keySet()) {
+            Map<String, Object> service = (Map<String, Object>) services.get(serviceName);
+
             // Add Docker image
-            service.put("image",DOCKER_IMAGE);
+            service.put("image", DOCKER_IMAGE);
 
             // Get environment
-            if(service.get("environment") == null) 
-                service.put("environment", new HashMap<String,Object>());
-            Map<String,Object> environment = (Map<String,Object>) service.get("environment");
-            
+            Map<String, Object> environment = (Map<String, Object>) service.get("environment");
+            if (environment == null) {
+                environment = new HashMap<String, Object>();
+                service.put("environment", environment);
+            }
+
             // Set TIMEOUT, PICK_PERCENTAGE, and FAIL_PERCENTAGE (if not there)
-            if(environment.get("TIMEOUT") == null)
+            if (environment.get("TIMEOUT") == null)
                 environment.put("TIMEOUT", DEFAULT_TIMEOUT);
-            if(environment.get("PICK_PERCENTAGE") == null)
+            if (environment.get("PICK_PERCENTAGE") == null)
                 environment.put("PICK_PERCENTAGE", DEFAULT_PICK_PERCENTAGE);
-            if(environment.get("FAIL_PERCENTAGE") == null)
+            if (environment.get("FAIL_PERCENTAGE") == null)
                 environment.put("FAIL_PERCENTAGE", DEFAULT_FAIL_PERCENTAGE);
-            
+
             // Set BACKEND_SERVICES
-            if(service.containsKey("depends_on")) {
-                List<String> backendServices = (List<String>) service.get("depends_on");
+            List<String> dependsOn = (List<String>) service.get("depends_on");
+            if (dependsOn != null) {
                 StringBuilder buffer = new StringBuilder();
-                buffer.append(backendServices.get(0));
-                for(int i=1; i<backendServices.size(); i++) {
+                buffer.append(dependsOn.get(0));
+                for (int i = 1; i < dependsOn.size(); i++) {
                     buffer.append(":");
-                    buffer.append(backendServices.get(i));
+                    buffer.append(dependsOn.get(i));
                 }
                 environment.put("BACKEND_SERVICES", buffer.toString());
             }
 
+            // Update depends_on
+            if (dependsOn == null)
+                dependsOn = new ArrayList<String>();
+            for (String elkService : elkServices.keySet())
+                dependsOn.add(elkService);
+            service.put("depends_on", dependsOn);
+
+            // Add deploy-replicas
+            Map<String, Object> deploy = (Map<String, Object>) service.get("deploy");
+            if (deploy == null) {
+                deploy = new HashMap<String, Object>();
+                service.put("deploy", deploy);
+            }
+            if (deploy.get("replicas") == null)
+                deploy.put("replicas", DEFAULT_REPLICAS);
+
             // Update port mappings (if any)
-            if(service.containsKey("ports")) {
+            if (service.containsKey("ports")) {
                 List<Object> ports = (List<Object>) service.get("ports");
                 List<String> portMappings = new ArrayList<String>();
-                for(Object port : ports) {
+                for (Object port : ports) {
                     portMappings.add(port.toString() + ":80");
                 }
                 service.put("ports", portMappings);
             }
 
             // Add GELF logging
-            Map<String,Object> logging = new HashMap<String,Object>();
+            Map<String, Object> logging = new HashMap<String, Object>();
             logging.put("driver", "gelf");
-            Map<String,Object> loggingOptions = new HashMap<String,Object>();
-            loggingOptions.put("gelf-address","udp://localhost:12201");
-            loggingOptions.put("tag",serviceName);
+            Map<String, Object> loggingOptions = new HashMap<String, Object>();
+            loggingOptions.put("gelf-address", "udp://localhost:12201");
+            loggingOptions.put("tag", serviceName);
             logging.put("options", loggingOptions);
         }
 
-        // Parse ELK YAML file...
-        InputStream elkYaml = null;
-        try {
-            elkYaml = new FileInputStream(new File(ELK_YAML));
-        } catch(FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        Map<String,Object> elk = yaml.load(elkYaml);
-        // ...and add ELK services
-        Map<String,Object> elkServices = (Map<String,Object>) elk.get("services"); 
-        for(String serviceName : elkServices.keySet())
+        // Add ELK services
+        for (String serviceName : elkServices.keySet())
             services.put(serviceName, elkServices.get(serviceName));
 
         // Output Docker Compose file
